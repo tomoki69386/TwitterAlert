f8d5ae365958abf61a7595dc919d594eb9004d3a
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-11-24 08:59:30 +0900

    Update

---
+++ TwitterAlert/Sources/TwitterAlert.swift
@@ -0,0 +1,14 @@
+//
+//  TwitterAlert.swift
+//  TwitterAlert
+//
+//  Created by 築山朋紀 on 2018/11/24.
+//  Copyright © 2018 tomoki. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+public class TwitterAlert: UIView {
+    
+}
---
+++ TwitterAlert/TwitterAlert.h
@@ -0,0 +1,19 @@
+//
+//  TwitterAlert.h
+//  TwitterAlert
+//
+//  Created by 築山朋紀 on 2018/11/24.
+//  Copyright © 2018 tomoki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TwitterAlert.
+FOUNDATION_EXPORT double TwitterAlertVersionNumber;
+
+//! Project version string for TwitterAlert.
+FOUNDATION_EXPORT const unsigned char TwitterAlertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TwitterAlert/PublicHeader.h>
+
+
