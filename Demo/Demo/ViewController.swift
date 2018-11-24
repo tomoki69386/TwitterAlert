4f86147fa72a257ca04cc817733e14e35b38c699
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-11-24 09:11:54 +0900

    Create

---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by 築山朋紀 on 2018/11/24.
+//  Copyright © 2018 tomoki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
--- TwitterAlert/Sources/AlertTableViewCell.swift
+++ TwitterAlert/Sources/AlertTableViewCell.swift
@@ -1,13 +1,9 @@
 //
-//  TwitterAlert.swift
+//  AlertTableViewCell.swift
 //  TwitterAlert
 //
 //  Created by 築山朋紀 on 2018/11/24.
 //  Copyright © 2018 tomoki. All rights reserved.
 //
 
-import UIKit
-
-public class TwitterAlert: UIView {
-    
-}
+import Foundation
