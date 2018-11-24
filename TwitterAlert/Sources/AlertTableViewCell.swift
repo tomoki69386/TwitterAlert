725ae55c37058413b19afae52cee8eb34085bd61
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-11-24 09:40:27 +0900

    Add AlertTableViewCell

--- TwitterAlert/Sources/AlertTableViewCell.swift
+++ TwitterAlert/Sources/AlertTableViewCell.swift
@@ -6,4 +6,29 @@
 //  Copyright © 2018 tomoki. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+public class AlertTableViewCell: UITableViewCell {
+    
+    override public init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+    }
+    
+    public func setUp() {
+        textLabel?.textColor = UIColor.black
+        textLabel?.text = "リツイート"
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override public func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override public func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
